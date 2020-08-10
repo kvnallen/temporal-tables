e57f16f9e487ac9a60faa170b5c17cb5bc4b8ac4
kevin <kevinallenlf@gmail.com>
2020-08-10 08:29:48 -0300

    added methods for create & update

--- TemporalTableCrud/Controllers/ProductsController.cs
+++ TemporalTableCrud/Controllers/ProductsController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TemporalTableCrud.Contexts;
@@ -11,13 +9,19 @@ namespace TemporalTableCrud.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly ProductDbContext context;
+
+        public ProductsController(ProductDbContext context)
+        {
+            this.context = context;
+        }
+
         [HttpGet]
-        public IActionResult Index([FromServices] ProductDbContext context,
-            DateTime? historyDateTime)
+        public IActionResult Index(DateTime? historyDateTime)
         {
             if (historyDateTime.HasValue)
             {
-                var sql = $"SELECT * FROM Products FOR SYSTEM_TIME FROM {{0}} TO {{1}}";
+                var sql = "SELECT * FROM Products FOR SYSTEM_TIME FROM {0} TO {1}";
                 var products = context.Products
                     .FromSqlRaw(sql, historyDateTime.Value.Date, historyDateTime.Value.Date.AddDays(1).AddTicks(-1))
                     .AsNoTracking()
@@ -30,9 +34,27 @@ namespace TemporalTableCrud.Controllers
         }
 
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult Create() => View(new Product { Id = 0 });
+
+        [HttpGet]
+        public IActionResult Edit(int productId)
         {
-            return View();
+            var product = context.Products.Find(productId);
+            return View("Create", product);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            context.Products.Update(product);
+
+            context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- TemporalTableCrud/Models/Product.cs
+++ TemporalTableCrud/Models/Product.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TemporalTableCrud.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
         public string Name { get; set; }
     }
 }
